90f271fd098e3d1f1c572b3f5c5683de1e83a0c8
tleza0258 <68152988+tleza0258@users.noreply.github.com>
2020-10-14 11:52:26 +0700

    Add files via upload

---
+++ array 2d-2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+void display(string name[], float price[5][3]) {
+	cout << left << setw(15) << "No." << setw(20) << "Product" << setw(20) << "Price" << setw(20) << "Vat7%" << setw(20) << "Net" << endl; 
+	for (int i = 0; i < 5; i++) {
+		cout << left << fixed << setw(15) << i+1 << setw(20) << name[i] << setw(20)<< setprecision(2) << price[i][0] << setw(20) << setprecision(2) << price[i][1] << setw(20) << setprecision(2) << price[i][2] << endl; 
+	}
+}
+
+
+void calprice(float price[5][3]) {
+	for (int i = 0; i < 5; i++) {
+		price[i][1] = (price[i][0] * 7/100);
+		price[i][2] = price[i][0] + price[i][1];
+	}
+}
+
+
+
+int main() {
+	string name[5];
+	float price[5][3];
+	for (int i = 0; i < 5; i++) {
+		cout << "Enter Product Name: ";
+		cin >> name[i];
+		cout << "\tprice: ";
+		cin >> price[i][0];
+	}
+	calprice(price);
+	display(name, price);
+	system("pause");
+	return 0;
+}
